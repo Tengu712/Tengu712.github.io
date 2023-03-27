607fd33684631bcc0a858c6f6c205a640e633e41
Tengu712 <genreihoutengu@gmail.com>
2023-03-28 07:21:11 +0900

    feat: add codeblock formatter

--- codeblock/codeblock.js
+++ codeblock/codeblock.js
@@ -1,8 +1,3 @@
-const newline = (_, codeblock) => {
-    codeblock.appendChild(document.createElement("br"));
-    return true;
-}
-
 function append_span(cls, text, codeblock) {
     const span = document.createElement("span");
     span.classList.add(cls);
@@ -11,16 +6,13 @@ function append_span(cls, text, codeblock) {
 }
 
 function match(code, codeblock, patterns) {
-    let i = 0;
     for (const pattern of patterns) {
         const res = code.match(pattern[0]);
         if (res === null) {
-            ++i;
             continue;
         }
         const start = res.indices[0][0];
         const end = res.indices[0][1];
-        console.log(i, end, code);
         if (end === 0) {
             append_span("error", "FORMATTING ERROR: ENDLESS LOOP", codeblock);
             return Number.MAX_SAFE_INTEGER;
@@ -29,38 +21,77 @@ function match(code, codeblock, patterns) {
         while (prev_end < start) {
             prev_end += match(code.slice(prev_end, start), codeblock, patterns);
         }
-        if (pattern[2] !== null && pattern[2](code, codeblock)) {
-            return end;
+        let matched = code.slice(start, end);
+        if (pattern[2] !== null) {
+            matched = pattern[2](matched, codeblock);
         }
-        append_span(pattern[1], code.slice(start, end), codeblock);
+        const splitted = matched.split('\n');
+        for (let i = 0; i < splitted.length; ++i) {
+            if (splitted[i].length !== 0)
+                append_span(pattern[1], splitted[i], codeblock);
+            if (i < splitted.length - 1)
+                codeblock.appendChild(document.createElement("br"));
+        }
         return end;
     }
     append_span("error", "FORMATTING ERROR: NO MATCH", codeblock);
     return Number.MAX_SAFE_INTEGER;
 }
 
+function get_prev_token(c) {
+    if (c !== null && (c.classList.contains("space") || c.tagName === "br"))
+        return get_prev_token(c.previousElementSibling);
+    else
+        return c;
+}
+
+const make_id_function = (code, codeblock) => {
+    const elem = get_prev_token(codeblock.lastChild);
+    if (elem.classList.contains("id"))
+        elem.classList.add("function");
+    return code;
+}
+
+const make_id_type = (code, codeblock) => {
+    const elem = get_prev_token(codeblock.lastChild);
+    if (elem.classList.contains("id"))
+        elem.classList.add("type");
+    if (code[0] !== '*')
+        return code;
+    const span = document.createElement("span");
+    span.classList.add("type");
+    span.innerText = "*";
+    codeblock.appendChild(span);
+    return code.slice(1);
+}
+
+const make_pointer_type = (code, codeblock) => {
+}
+
 function patterns_latex() {
     return [
-        [/\n/sd, "", newline],
-        [/^%.*/sd, "comment", null],
-        [/^[+-]?\d+(\.\d+)?(pt|in|cm|mm)/sd, "literal", null],
-        [/\\[a-z]+/sd, "type", null],
+        [/%[^\n]*\n/sd, "comment", null],
         [/{|}|\[|\]|=|,/sd, "plain", null],
+        [/[+-]?\d+(\.\d+)?(pt|in|cm|mm)/sd, "literal", null],
+        [/\\[a-z]+/sd, "type", null],
         [/.+/sd, "plain", null],
     ];
 }
 
 function patterns_c() {
     return [
-        [/\n/sd, "", newline],
-        [/^\/\/.*/sd, "comment", null],
-        [/^#.*/sd, "comment", null],
-        [/^"[^(\\")]*"/sd, "string", null],
-        [/^[+-]?\d+(\.\d+)?(f|)/sd, "literal", null],
-        [/^(if|for|while|return|continue|break)/sd, "keyword", null],
-        [/^(void|const|static|extern|signed|unsigned|char|short|int|long|float|double)/sd, "type", null],
-        [/\(|\)|{|}|\[|\]|\+|-|\*|\/|=|>|<|!|.|,|:|;/sd, "plain", null],
-        [/\s+/sd, "plain", null],
+        [/"([^\n\\]|\\[^\n])*"/sd, "string", null],
+        [/\/\/[^\n]*\n/sd, "comment", null],
+        [/\/\*([^\*]|\*[^\\])*\*\//sd, "comment", null],
+        [/#[^\n]*\n/sd, "comment", null],
+        [/[\s\n]+/sd, "space", null],
+        [/\(/sd, "plain", make_id_function],
+        [/\)|{|}|\[|\]|,|:|;/sd, "plain", null],
+        [/^(if|for|while|switch|case|return|continue|break|sizeof)/sd, "keyword", null],
+	[/\*?[a-zA-Z_][a-zA-Z0-9_]*\*?/sd, "id", make_id_type],
+        [/[+-]?\d+(\.\d+)?(f|)/sd, "literal", null],
+        [/\+|-|\*|\/|=|>|<|!/sd, "operator", null],
+	[/\./sd, "plain", null],
         [/.+/sd, "plain", null],
     ];
 }
@@ -77,6 +108,8 @@ window.onload = function () {
                 patterns = patterns_c();
                 break;
         }
+        if (patterns === null)
+            continue;
         let code = codeblock.innerText;
         codeblock.innerText = "";
         while (code.length > 0) {
