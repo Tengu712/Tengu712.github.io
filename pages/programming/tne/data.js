2fd77aa5b06149d23eb898ba4ddbe57d76ac0372
Tengu712 <genreihoutengu@gmail.com>
2023-04-14 00:33:18 +0900

    feat: tiny novelgame engine

---
+++ pages/programming/tne/data.js
@@ -0,0 +1,23 @@
+const z = [
+    false,
+    {
+        a: "bg-title.png",
+        b: "",
+        c: "",
+        d: "",
+        e: "",
+        f: 2,
+        g: 0,
+        h: 0,
+    },
+    {
+        a: "bg-game.png",
+        b: "chara.png",
+        c: "ここにテキストを入力する",
+        d: "",
+        e: "",
+        f: 0,
+        g: 0,
+        h: 0,
+    },
+];
