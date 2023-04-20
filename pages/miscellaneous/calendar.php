2f496ac31323d24c543d7745f4c2fdad9c019f1e
Tengu712 <genreihoutengu@gmail.com>
2023-04-20 22:59:43 +0900

    fix: change font size in phone

--- pages/miscellaneous/calendar.php
+++ pages/miscellaneous/calendar.php
@@ -4,6 +4,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
 <title>Calendar</title>
 <style>
+    @media screen and (max-width: 1000px) {
+        body {
+            font-size: 0.5em;
+        }
+    }
     table {
         border-collapse: collapse;
         width: 100%;
@@ -45,22 +50,22 @@
     for ($year = 23; $year <= 24; $year += 1) {
         for ($month = 1; $month <= 12; $month += 1) {
             if ($year === 23 && $month < 4) continue;
-	    echo '<h1>20', $year, '年', $month, '月</h1>';
+        echo '<h1>20', $year, '年', $month, '月</h1>';
             echo '<table id="', $year, '/', $month,'"><tr>';
             for ($i = 0; $i < 7; $i += 1) {
                 echo '<th>', $dow[$i], '</th>';
             }
             echo '</tr><tr>';
-	    for ($i = 0; $i < $cnt % 7; $i += 1) {
+        for ($i = 0; $i < $cnt % 7; $i += 1) {
                 echo '<td class="empty"></td>';
-	    }
+        }
             for ($day = 1; $day <= 31; $day += 1) {
                 if ($day > 30 && ($month === 4 || $month === 6 || $month === 9 || $month === 11)) break;
                 if ($day > 28 && $month === 2 && ($year === 25 || $year === 26)) break;
                 if ($day > 29 && $month === 2 && ($year === 24)) break;
                 if ($cnt % 7 === 0) echo '</tr><tr>';
                 echo '<td><p class="day">', $day, '</p>';
-		echo '<p>';
+                echo '<p>';
                 if (isset($json[$year][$month][$day])) {
                     foreach ($json[$year][$month][$day] as $content) {
                         echo $content, '<br>';
@@ -70,11 +75,11 @@
                 $cnt += 1;
             }
             if ($cnt % 7 !== 0) {
-	        for ($i = $cnt % 7; $i < 7; $i += 1) {
+            for ($i = $cnt % 7; $i < 7; $i += 1) {
                     echo '<td class="empty"></td>';
                 }
             }
-	    echo '</tr></table>';
+        echo '</tr></table>';
         }
     }
 
