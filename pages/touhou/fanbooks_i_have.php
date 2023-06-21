82bced9173d2d2a960fd2072a27a7b4590cd54d3
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2023-06-21 19:10:29 +0900

    feat: add fanbooks

--- pages/touhou/fanbooks_i_have.php
+++ pages/touhou/fanbooks_i_have.php
@@ -9,7 +9,7 @@
     body {
       background-color: black;
       color: white;
-      margin: 1em;
+      margin: 1rem;
     }
 
     a {
@@ -20,7 +20,7 @@
       color: red;
     }
 
-    p {
+    p.right {
       text-align: right;
     }
 
@@ -31,14 +31,18 @@
 
     td,
     th {
-      border-bottom: solid 1px white;
-      border-top: solid 1px white;
-      padding: 0.25em;
+      text-align: left;
+      padding: 0.25rem;
     }
 
-    td.newcircle,
     th {
-      border-top: solid 4px white;
+      border-top: 1px solid white;
+      border-bottom: 1px solid white;
+    }
+
+    td.spacer {
+      border-top: 1px solid #555;
+      border-bottom: 1px solid #555;
     }
 
     @media screen and (max-width: 1000px) {
@@ -54,7 +58,8 @@
 <body>
   <h1>紙媒体で所有する・かつ既読の東方同人誌</h1>
 
-  <p>2022/8/9更新</p>
+  <p>「∀本∈{表中にある}, 紙媒体として所有している∧読了している」であって、<br>「∀本∈{紙媒体として所有している∧読了している}, 表中にある」でないことに留意せよ。</p>
+  <p class="right">2023/6/21更新</p>
   <center>
     <table>
       <tr>
@@ -68,24 +73,25 @@
       $tmp_person = '';
       foreach (file('./fanbooks_i_have.dat') as $line) {
         $contents = explode('|', $line);
-        $newcircle = $contents[0] != $tmp_circle ? 'newcircle' : '';
+        if ($contents[0] != $tmp_circle) {
+          echo '<tr><td class="spacer">　</td>',
+              '<td class="spacer hidable"></td>',
+              '<td class="spacer"></td>',
+              '<td class="spacer hidable"></td></tr>';
+        }
         echo '<tr>';
-        echo '<td class="', $newcircle, '">';
+        echo '<td>';
         if ($contents[0] != $tmp_circle) {
           echo $contents[0];
         }
         echo '</td>';
-        echo '<td class="hidable ', $newcircle, '">';
+        echo '<td class="hidable">';
         if ($contents[1] != $tmp_person) {
           echo $contents[1];
         }
         echo '</td>';
-        echo '<td class="', $newcircle, '">';
-        echo $contents[2];
-        echo '</td>';
-        echo '<td class="hidable ', $newcircle, '">';
-        echo $contents[3];
-        echo '</td>';
+        echo '<td>', $contents[2], '</td>';
+        echo '<td class="hidable">', $contents[3], '</td>';
         echo '</tr>';
         $tmp_circle = $contents[0];
         $tmp_person = $contents[1];
