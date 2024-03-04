951da9de378e126927fc60d463dd63b2e654a889
Tengu712 <genreihoutengu@gmail.com>
2024-03-05 00:11:24 +0900

    feat: change codeblock highlight theme

--- src/coms/codeblock.js
+++ src/coms/codeblock.js
@@ -1,9 +1,9 @@
 const parser = require("../parser")
 
-const CSS = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css">
+const CSS = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
   <script>hljs.configure({languages: []}); hljs.initHighlightingOnLoad();</script>
-  <link rel="stylesheet" type="text/css" href="/coms/codeblock.css?20240213">`
+  <link rel="stylesheet" type="text/css" href="/coms/codeblock.css?20240305">`
 
 exports.run = function(text, eohs) {
   const matcheds = parser.parseTag(text, "Codeblock")
