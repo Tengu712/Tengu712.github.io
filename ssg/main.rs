dc2c64eca37427f28b5150227cafccf4353615b9
Tengu712 <genreihoutengu@gmail.com>
2025-11-28 23:12:33 +0900

    file

--- ssg/main.rs
+++ ssg/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
+struct Page {
+    body: String,
+}
+
+impl Page {
+    fn from(path: &str) -> Self {
+        Self {
+            body: fs::read_to_string(path).unwrap(),
+        }
+    }
+}
+
 fn main() {
-    println!("{}", markdown::to_html("## Hi, *Saturn*! 🪐"));
+    println!("{}", markdown::to_html(&Page::from("pages/index.md").body));
 }
