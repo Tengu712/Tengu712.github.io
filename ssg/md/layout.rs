228a256d4b4e0a6cdf7a6ef103676aa32d5eb137
Tengu712 <genreihoutengu@gmail.com>
2025-11-29 20:36:49 +0900

    footer

--- ssg/md/layout.rs
+++ ssg/md/layout.rs
@@ -14,10 +14,14 @@ const HEADER: &str = "\
 ";
 const HEADER_STYLE: &str = include_str!("../../asset/style/header.css");
 
+const FOOTER: &str = "<div class=\"footer\">2022-2025, Tengu712, Skydog Association</div>";
+const FOOTER_STYLE: &str = include_str!("../../asset/style/footer.css");
+
 fn to_html_post(content: &Node, styles: &mut HashSet<StrPtr>, buf: &mut String) {
     const STYLE_PC: &str = include_str!("../../asset/style/triad.css");
     styles.insert(StrPtr(STYLE_PC));
     styles.insert(StrPtr(HEADER_STYLE));
+    styles.insert(StrPtr(FOOTER_STYLE));
 
     buf.push_str(HEADER);
     buf.push_str("<div class=\"triad\">");
@@ -28,6 +32,7 @@ fn to_html_post(content: &Node, styles: &mut HashSet<StrPtr>, buf: &mut String) 
     buf.push_str("<div class=\"triad-side\">");
     buf.push_str("</div>");
     buf.push_str("</div>");
+    buf.push_str(FOOTER);
 }
 
 pub fn to_html(layout: &str, content: &Node, styles: &mut HashSet<StrPtr>) -> String {
