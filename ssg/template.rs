37b862f127c90926bbb33d30ec9449b940fd0382
Tengu712 <genreihoutengu@gmail.com>
2025-12-01 01:24:32 +0900

    comprescss

--- ssg/template.rs
+++ ssg/template.rs
@@ -3,6 +3,25 @@
 use crate::strutil::StrPtr;
 use std::collections::HashSet;
 
+fn push_css(css: &str, buf: &mut String) {
+    let mut space_count = 0;
+    for c in css.chars() {
+        match c {
+            '\n' => (),
+            ' ' => space_count += 1,
+            c if space_count == 1 => {
+                buf.push(' ');
+                buf.push(c);
+                space_count = 0;
+            }
+            c => {
+                buf.push(c);
+                space_count = 0;
+            }
+        }
+    }
+}
+
 pub fn generate_html_string(styles: &HashSet<StrPtr>, title: &str, body: &str) -> String {
     const HTML_STYLE: &str = "\
         <!DOCTYPE html>\
@@ -30,7 +49,7 @@ pub fn generate_html_string(styles: &HashSet<StrPtr>, title: &str, body: &str) -
     let mut buf = String::new();
     buf.push_str(HTML_STYLE);
     for style in styles.iter() {
-        buf.push_str(style.0);
+        push_css(style.0, &mut buf);
     }
     buf.push_str(STYLE_TITLE);
     buf.push_str(title);
